994f2a7710a8c4f7423a0b1a69b9c012bd5a9c1c
chandradattpandey <pandeyc644@gmail.com>
2020-03-14 21:41:41 +0530

    14/3

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,26 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
+// var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+
+
+// facebook
+
 passport.use(new FacebookStrategy({
   clientID: '198234414841456',
   clientSecret: '057e72542b4534f7b93bc6c3a9d628d8',
   callbackURL: "https://chandra-verify.herokuapp.com/auth/facebook/callback",
-  profileFields: ['id', 'displayName', 'photos', 'email','profileUrl']
+  profileFields: ['id', 'displayName', 'photos', 'email', 'profileUrl']
 },
-function(accessToken, refreshToken, profile, cb){
+  function (accessToken, refreshToken, profile, cb) {
 
- return cb(null,profile);
-}
+    return cb(null, profile);
+  }
 ));
 
 
-router.get('/', function(req, res, next) {
-  res.render('register.html');
+router.get('/', function (req, res, next) {
+  res.render('reg.html');
 });
 
 
@@ -25,11 +30,38 @@ router.get('/auth/facebook', passport.authenticate('facebook'));
 
 
 
-router.get('/auth/facebook/callback',passport.authenticate('facebook', { 
-  failureRedirect: '/login' }),(req,res)=>{
-    console.log("PPPPPPPPPPPPPPPPPPPPPPPP",req.user)
-    res.render('reg.html');
-  });
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  failureRedirect: '/login'
+}), (req, res) => {
+  console.log("PPPPPPPPPPPPPPPPPPPPPPPP", req.user);
+  res.send('Success Loged In')
+});
+
+
+
+// google
+
+
+// passport.use(new GoogleStrategy({
+//   clientID: GOOGLE_CLIENT_ID,
+//   clientSecret: GOOGLE_CLIENT_SECRET,
+//   callbackURL: "http://www.example.com/auth/google/callback"
+// },
+//   function (accessToken, refreshToken, profile, done) {
+
+//     return done(err, user);
+//   }
+// ));
+
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function (req, res) {
+//     res.redirect('/');
+//   });
+
 
 
 module.exports = router;
