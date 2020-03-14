5014719e376b3b8cfcb0cb513ae1a5336cfebd2f
chandradattpandey <pandeyc644@gmail.com>
2020-03-15 05:05:52 +0530

    876

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/auth/facebook', passport.authenticate('facebook',{ scope: ['email', 'profile'] }));
+router.get('/auth/facebook', passport.authenticate('facebook',{ scope: ['email'] }));
 
 
 
